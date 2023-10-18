b22e5088cf857a0e56d006c8a64a0fe1f1cf9dbd
annisarisma <annisarisma117@gmail.com>
2023-10-18 10:02:51 +0700

    Add: register view, controller and route ref #1

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,8 +17,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/register', function () {
-    return view('authentication.register', with([
-        'title' => 'Register'
-    ]));
+
+Route::controller(UserController::class)->group(function () {
+    Route::get('/register', 'index');
+    Route::post('/register/store', 'store');
 });
